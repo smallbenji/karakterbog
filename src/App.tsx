c18f64c5fabef2058b4e0c1244079212657507ce
smallbenji <benjaminkfalch@gmail.com>
2024-07-30 14:57:05 +0200

    Added buttons to the input form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router";
+
 const App = () => {
+	const navigate = useNavigate();
 	return (
 		<>
-			<h1>Karakterbog</h1>
+			<Button
+				variant="contained"
+				onClick={() => {
+					navigate("/lærer");
+				}}
+			>
+				Lærer
+			</Button>
+			<Button
+				variant="contained"
+				onClick={() => {
+					navigate("/elev/1");
+				}}
+			>
+				Elev (Benjamin Falch)
+			</Button>
+			<Button
+				variant="contained"
+				onClick={() => {
+					navigate("/elev/2");
+				}}
+			>
+				Elev (Malthe Guldkage)
+			</Button>
+			<Button
+				variant="contained"
+				onClick={() => {
+					navigate("/elev/3");
+				}}
+			>
+				Elev (Flemming Jensen)
+			</Button>
 		</>
 	);
 };
